f060a6ec63f5439de1467b038c4f5b2e9ec383c2
arthurmax21 <arthurmax147@gmail.com>
2025-06-03 21:39:38 -0300

    Inicio da aula de introdução(01) de Javascript

---
+++ JS/JS01-Introducao/main.js
@@ -0,0 +1,29 @@
+var idade
+console.log("Olá mundo!") // Apresentar uma mensagem
+console.clear() // Limpar a tela de saída
+// 10 número inteiro
+// 3.14 número de ponto flutuante
+
+// Variáveis:
+var numero_1 = 192 // Inicializar uma variável (dar logo de cara o valor a variável)
+console.log(numero_1)
+
+var num1 = 4
+var num2 = 4
+var resultado = num1 + num2
+console.log(resultado)
+
+idade = 89 // Iniciar a variável em outra parte do código
+console.log(idade)
+
+// Textos - Strings
+var nome = "Matue"
+var nome2 = 'Tuê'
+var nome3 = '129312.39308' // será lido como texto
+console.log(nome, nome2, nome3) // Apresenta tudo num 'log' só
+
+VAR // Flexível e com menos regras, posso alterar o valor inicial de uma 'var' e recriá-la em outras partes do código)
+
+LET // Não pode recriar a mesma em outras partes do código com o mesmo nome, mas pode chamar e alterar o seu valor inicial)
+ 
+CONST // Não pode recriar a mesma em outras partes do código e não pode alterar o valor inicial, garantindo a confiança do dado que está sendo inserido, ao decorrer do código(garantindo que você perca o valor incial ao decorrer do código)
